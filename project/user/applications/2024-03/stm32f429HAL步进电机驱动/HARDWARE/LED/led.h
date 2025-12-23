2dde6c655c78f33962f14892ad41df5255455808
Shilai Li <2649730335git@gmail.com>
2025-12-24 01:36:01 +0800

    ,

--- project/user/applications/2024-03/stm32f429HAL步进电机驱动/HARDWARE/LED/led.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LED_H
-#define _LED_H
-#include "sys.h"
-
-#define LED0 PBout(1)   //LED0
-#define dir PBout(0)   //LED1
-#define pwm PBout(0)   //LED1
-void LED_Init(void);
-#endif
--- project/user/drivers/tests/ssd1306_custom_tests.c
+++
@@ -1,9 +0,0 @@
-#include "ssd1306_custom.h"
-
-void User_Entry(void)
-{
-    while (1)
-    {
-        ssd1306_TestChineseDisplay();
-    }
-}
