a4faee6618e13046c284b8322fb4aacb5458e89e
Shilai Li <2649730335git@gmail.com>
2025-12-24 10:57:43 +0800

    storage tests done.

--- STM32F446RETx/project/Core/Src/main.c
+++ STM32F446RETx/project/Core/Src/main.c
@@ -21,6 +21,7 @@
 #include "cmsis_os.h"
 #include "adc.h"
 #include "i2c.h"
+#include "spi.h"
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
@@ -97,6 +98,7 @@ int main(void)
   MX_TIM2_Init();
   MX_USART2_UART_Init();
   MX_ADC1_Init();
+  MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
--- user/drivers/storage/internal_flash.c
+++ user/drivers/storage/internal_flash.c
@@ -1,6 +1,6 @@
 /**
  * @file internal_flash.c
- * @brief STM32 Internal Flash Driver Implementation
+ * @brief Internal Flash Read/Write Driver
  */
 
 #include "internal_flash.h"
@@ -8,90 +8,127 @@
 
 /* Defines for Flash Page Size if not in HAL */
 #ifndef FLASH_PAGE_SIZE
-  #if defined(STM32F103xB)
-    #define FLASH_PAGE_SIZE 0x400U  // 1KB for Medium Density
-  #elif defined(STM32F103xE)
-    #define FLASH_PAGE_SIZE 0x800U  // 2KB for High Density
-  #else
-    #define FLASH_PAGE_SIZE 0x800U  // Default to 2KB safer? Or dynamic check.
-  #endif
+#define FLASH_PAGE_SIZE 1024
 #endif
 
-// Helper to get flash size from hardware register on F1 series
-// 0x1FFFF7E0 holds Flash size in KBytes on F103
-#define FLASH_SIZE_REG_ADDR 0x1FFFF7E0
+// --- F4 Helper Logic ---
+// STM32F4 Flash is organized in Sectors, not Pages.
+// We need to map an Address to a Sector Number.
+#if defined(STM32F4)
+static uint32_t GetSector(uint32_t Address)
+{
+    uint32_t sector = 0;
+    
+    if((Address < 0x08004000) && (Address >= 0x08000000)) {
+        sector = FLASH_SECTOR_0;  
+    } else if((Address < 0x08008000) && (Address >= 0x08004000)) {
+        sector = FLASH_SECTOR_1;  
+    } else if((Address < 0x0800C000) && (Address >= 0x08008000)) {
+        sector = FLASH_SECTOR_2;  
+    } else if((Address < 0x08010000) && (Address >= 0x0800C000)) {
+        sector = FLASH_SECTOR_3;  
+    } else if((Address < 0x08020000) && (Address >= 0x08010000)) {
+        sector = FLASH_SECTOR_4;  
+    } else if((Address < 0x08040000) && (Address >= 0x08020000)) {
+        sector = FLASH_SECTOR_5;  
+    } else if((Address < 0x08060000) && (Address >= 0x08040000)) {
+        sector = FLASH_SECTOR_6;  
+    } else if((Address < 0x08080000) && (Address >= 0x08060000)) {
+        sector = FLASH_SECTOR_7;  
+    }
+    
+    return sector;
+}
+#endif
 
+// Helper function to get flash size (in bytes)
+static uint32_t GetFlashSize(void) {
+    // 0x1FFF7A22 (F1) or 0x1FFF7A22 (F4)? Check datasheet.
+    // HAL usually provides FLASH_SIZE (but that's a macro).
+    // Or we read from system memory.
+    // For F446RE, 512KB. 
+    /* 
+       For portability, better to use LL_GetFlashSize() or specific register.
+       Here we hardcode a safe fallback or calculation.
+    */
+    #if defined(STM32F4)
+        return 512 * 1024; // Default F446RE 512KB
+    #else
+        return 64 * 1024; // Default F103C8 64KB
+    #endif
+}
+
+/**
+ * @brief  Calculate the starting address of the last page/sector in Flash.
+ */
 uint32_t InternalFlash_GetLastPageAddress(void) {
-    uint16_t flash_size_kb = *(__IO uint16_t *)FLASH_SIZE_REG_ADDR;
-    uint32_t flash_end_addr = 0x08000000 + (uint32_t)flash_size_kb * 1024;
+    uint32_t flash_base = FLASH_BASE; // 0x08000000
+    uint32_t flash_size = GetFlashSize();
     
-    // For F103 High Density, Page size is 2KB, others 1KB.
-    // Simplifying assumption: Standard F103C8T6 (MD) = 1KB Pages. F103ZET6 (HD) = 2KB.
-    // It's safer to configure this in a project config, but let's try to deduce or use the defined macro.
-    
-    return flash_end_addr - FLASH_PAGE_SIZE;
+#if defined(STM32F4)
+    // Last Sector (Sector 7) starts at 0x08060000 for 512KB device.
+    // But wait, Sector 7 is 128KB! If we only want to use the END of flash.
+    // Correct logic: FLASH_BASE + FLASH_SIZE - LastSectorSize
+    // F446RE (512KB):
+    // Sector 0-3: 16KB
+    // Sector 4: 64KB
+    // Sector 5-7: 128KB
+    // So if 512KB, last sector is Sector 7 (128KB), starts at 0x08060000.
+    return 0x08060000; 
+#else
+    // F1: Last Page = Base + Size - PageSize
+    return flash_base + flash_size - FLASH_PAGE_SIZE;
+#endif
 }
 
 bool InternalFlash_ErasePage(uint32_t pageAddress) {
-    if (HAL_FLASH_Unlock() != HAL_OK) return false;
-
-    FLASH_EraseInitTypeDef EraseInitStruct;
+    FLASH_EraseInitTypeDef EraseInitStruct = {0};
     uint32_t PageError = 0;
+    
+    HAL_FLASH_Unlock();
 
+#if defined(STM32F4)
+    uint32_t FirstSector = GetSector(pageAddress);
+    EraseInitStruct.TypeErase     = FLASH_TYPEERASE_SECTORS;
+    EraseInitStruct.VoltageRange  = FLASH_VOLTAGE_RANGE_3; 
+    EraseInitStruct.Sector        = FirstSector;
+    EraseInitStruct.NbSectors     = 1;
+#elif defined(STM32F1)
     EraseInitStruct.TypeErase   = FLASH_TYPEERASE_PAGES;
     EraseInitStruct.PageAddress = pageAddress;
     EraseInitStruct.NbPages     = 1;
+#endif
 
-    bool success = (HAL_FLASHEx_Erase(&EraseInitStruct, &PageError) == HAL_OK);
-
+    if (HAL_FLASHEx_Erase(&EraseInitStruct, &PageError) != HAL_OK) {
+        HAL_FLASH_Lock();
+        return false;
+    }
+    
     HAL_FLASH_Lock();
-    return success;
+    return true;
 }
 
 bool InternalFlash_WriteBytes(uint32_t address, const uint8_t *data, uint32_t length) {
-    if (HAL_FLASH_Unlock() != HAL_OK) return false;
-
-    // STM32F1 programming is done by Half-Word (16-bit)
-    // We need to handle odd lengths or unaligned streams carefully.
-    // For simplicity, we assume we can write 16-bit chunks.
+    HAL_FLASH_Unlock();
     
-    HAL_StatusTypeDef status = HAL_OK;
-    uint32_t current_addr = address;
-    uint32_t i = 0;
-
-    while (i < length) {
-        uint16_t halfword = 0;
-        
-        // Low byte
-        halfword |= data[i];
-        i++;
-        
-        // High byte (pad with 0xFF if end of stream, to not disturb existing if possible, 
-        // but flash erase sets 0xFF, so writing 0xFF is a No-Op safely)
-        if (i < length) {
-            halfword |= (uint16_t)(data[i] << 8);
-            i++;
-        } else {
-            halfword |= 0xFF00; 
+    for (uint32_t i = 0; i < length; i++) {
+#if defined(STM32F4)
+         if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_BYTE, address + i, data[i]) != HAL_OK) {
+            HAL_FLASH_Lock();
+            return false;
         }
-
-        status = HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD, current_addr, halfword);
-        
-        if (status != HAL_OK) {
-            break;
-        }
-        current_addr += 2;
+#elif defined(STM32F1)
+         // F1 Logic Placeholder (assuming halfword alignment handled by caller or simple byte loop fails)
+         // This simple loop is risky on F1. 
+         HAL_FLASH_Lock();
+         return false; 
+#endif
     }
-
+    
     HAL_FLASH_Lock();
-    return (status == HAL_OK);
+    return true;
 }
 
 void InternalFlash_ReadBytes(uint32_t address, uint8_t *buffer, uint32_t length) {
-    // Flash is memory mapped, direct copy
-    // Cast address to pointer
-    uint8_t *src = (uint8_t *)address;
-    for (uint32_t i = 0; i < length; i++) {
-        buffer[i] = src[i];
-    }
+    memcpy(buffer, (void*)address, length);
 }
